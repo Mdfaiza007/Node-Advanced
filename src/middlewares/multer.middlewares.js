a1123f31610d9ddb2dc23740f21fd04df374dce5
MdFaiza007 <mdfaizan35609@gmail.com>
2026-03-05 04:07:29 +0530

    upload file

---
+++ src/middlewares/multer.middlewares.js
@@ -0,0 +1,23 @@
+import multer from "multer";
+
+/*
+Multer is a node.js middleware for handling multipart/form-data, 
+which is primarily used for uploading files.
+ It is written on top of busboy for maximum efficiency.
+
+NOTE: Multer will not process any form which is not multipart (multipart/form-data).
+
+*/
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+export const upload = multer({ 
+    storage, 
+})
